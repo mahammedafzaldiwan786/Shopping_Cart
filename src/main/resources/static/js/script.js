b9e3bf8b1264514c515b63e1b6afac4e3b223087
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2026-01-09 21:11:55 +0530

    light/dark theme implemented

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -287,3 +287,30 @@ jQuery.validator.addMethod('lettersonly', function(value, element) {
 	jQuery.validator.addMethod('numericOnly', function(value, element) {
 		return /^[0-9]+$/.test(value);
 	});
+	
+	
+	
+	
+	//theme
+	// =======================
+	// DARK / LIGHT THEME TOGGLE
+	// =======================
+
+	const themeToggle = document.getElementById("themeToggle");
+
+	if (localStorage.getItem("theme") === "dark") {
+	    document.body.classList.add("dark-theme");
+	}
+
+	function toggleTheme() {
+	    document.body.classList.toggle("dark-theme");
+
+	    if (document.body.classList.contains("dark-theme")) {
+	        localStorage.setItem("theme", "dark");
+	    } else {
+	        localStorage.setItem("theme", "light");
+	    }
+	}
+
+	
+	
